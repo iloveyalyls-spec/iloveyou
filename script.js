df88c3ce04149507a1a5b340d5b593ad05189f7b
bxntsze <bencebox20077@gmail.com>
2025-12-23 23:34:47 +0100

    majdnem finished

---
+++ script.js
@@ -0,0 +1,14 @@
+function updateScale() {
+  const scale = Math.min(
+    window.innerWidth / 1920,
+    window.innerHeight / 1080
+  );
+  document.documentElement.style.setProperty(
+    '--scale',
+    Math.min(Math.max(scale, 0.4), 1.5)
+  );
+}
+
+window.addEventListener('resize', updateScale);
+updateScale();
+
